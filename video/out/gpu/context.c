001f0289013bb578815753a39477909203a924f2
yuygfgg <140488233+yuygfgg@users.noreply.github.com>
2026-04-20 15:15:32 +0800

    Starmine: vendor MoltenVK and Swift build patches

--- video/out/gpu/context.c
+++ video/out/gpu/context.c
@@ -50,6 +50,7 @@ extern const struct ra_ctx_fns ra_ctx_vulkan_xlib;
 extern const struct ra_ctx_fns ra_ctx_vulkan_android;
 extern const struct ra_ctx_fns ra_ctx_vulkan_display;
 extern const struct ra_ctx_fns ra_ctx_vulkan_mac;
+extern const struct ra_ctx_fns ra_ctx_vulkan_moltenvk;
 
 /* Direct3D 11 */
 extern const struct ra_ctx_fns ra_ctx_d3d11;
@@ -92,6 +93,9 @@ static const struct ra_ctx_fns *const contexts[] = {
 #endif
 #if HAVE_X11
     &ra_ctx_vulkan_xlib,
+#endif
+#if HAVE_MOLTENVK
+     &ra_ctx_vulkan_moltenvk,
 #endif
 #if HAVE_COCOA && HAVE_SWIFT
     &ra_ctx_vulkan_mac,
